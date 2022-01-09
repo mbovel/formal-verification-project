22ead2f9a4e4d2bd5bf592f8755dfe9686195273
Matt Bovel <matthieu@bovel.net>
2022-01-09 19:39:11 +0100

    Add loop and ADT examples

--- stainless-examples/07_loop.scala
+++ stainless-examples/07_loop.scala
@@ -1,12 +1,14 @@
 object Times {
+    // FIXME
     def decreasingLoop(n: Int): Int = {
-        require(n >= 0)
+        require(n >= 0 && n <= 10)
         var i: Int = n
         var sum: Int = 0
-        while(i < 10) {
+        (while (i < 10) {
+            decreases(- i)
             sum += n
             i += 1
-        }
-        n
+        }).invariant(0 <= i && i <= 10)
+        sum
     } ensuring(res => res >= 4)
 }
