5632ec453b94b85949fb8e38a422317b38e4fae6
Matt Bovel <matthieu@bovel.net>
2022-01-12 23:59:02 +0100

    Remove old stainless-examples folder

--- stainless-examples/02_twice.scala
+++
@@ -1,6 +0,0 @@
-object Twice {
-    def twice(x: Int): Int = {
-        require(x <= 100)
-        x * 2
-    } ensuring(res => res >= x)
-}
--- stainless-examples/03_times.scala
+++
@@ -1,5 +0,0 @@
-object Times {
-    def universalLaw(n: Int): Unit = {
-        assert(n * n >= 10)
-    }
-}
